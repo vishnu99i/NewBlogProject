38eef6ec32db38f1c13bfee461bacb9a5833d3fa
Vishnu P <vinventor1999@gmail.com>
2024-09-03 22:47:46 +0530

    signin

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -28,16 +28,16 @@ function Login() {
 
   return (
     <div
-    className='flex items-center justify-center w-full'
+    className='flex items-center justify-center text-sm mb-3 mt-3'
     >
-        <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+        <div className={`mx-auto w-full max-w-lg bg-gray-900 rounded-xl p-10 border border-black/10`}>
         <div className="mb-2 flex justify-center">
                     <span className="inline-block w-full max-w-[100px]">
                         <Logo width="100%" />
                     </span>
         </div>
         <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
-        <p className="mt-2 text-center text-base text-black/60">
+        <p className="mt-2 text-center text-base">
                     Don&apos;t have any account?&nbsp;
                     <Link
                         to="/signup"
