e18b56d2621b3d22ae64884dadd524547528818b
Vishnu P <vinventor1999@gmail.com>
2024-09-03 22:53:48 +0530

    signup and font

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -27,15 +27,15 @@ function Signup() {
     }
 
   return (
-    <div className="flex items-center justify-center">
-            <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+    <div className="flex items-center justify-center text-sm">
+            <div className={`mx-auto w-full max-w-lg bg-gray-900 rounded-xl p-10 border border-black/10`}>
             <div className="mb-2 flex justify-center">
                     <span className="inline-block w-full max-w-[100px]">
                         <Logo width="100%" />
                     </span>
                 </div>
                 <h2 className="text-center text-2xl font-bold leading-tight">Sign up to create account</h2>
-                <p className="mt-2 text-center text-base text-black/60">
+                <p className="mt-2 text-center text-base mb-1">
                     Already have an account?&nbsp;
                     <Link
                         to="/login"
